180f8c44fcc6bbd1e0a4877c6ced85afb3c7f0a8
yelsharawy <yelsharawy20@stuy.edu>
2022-10-16 09:15:10 -0400

    Added more cards to widgets

--- static/js/render_widgets.js
+++ static/js/render_widgets.js
@@ -3,19 +3,29 @@ container = $("#widget-container");
 function displayWidget(activeWidget, isHost=false) {
     switch (activeWidget.widget_type) {
         case "text":
-            e = $("<h2>").append(activeWidget.contents);
+            e = $("<h2>")
+                .attr("class", "main-center-card card")
+                .append(activeWidget.contents);
             break;
         case "image":
-            e = $("<img>").attr("src", activeWidget.contents);
+            e = $("<img>")
+            .attr("class", "main-center-card card")
+                .attr("src", activeWidget.contents);
             break;
         case "text_input":
             e = $("<div>")
-                .append($("<h2>").append(activeWidget.contents.prompt))
+                .append(
+                    $("<h2>")
+                        .attr("class", "main-center-card card")
+                        .append(activeWidget.contents.prompt)
+                )
             if (!isHost) {
                 e.append($("<br>"))
                 .append(
                     $("<form>").attr("action", "")
-                        .append("<input>").attr("type", "text").attr("name", "response")
+                        .append("<input>")
+                            .attr("type", "text")
+                            .attr("name", "response")
                     )
                 .append("<input>").attr("type", "submit")
             }
@@ -44,9 +54,11 @@ function displayWidget(activeWidget, isHost=false) {
             e.append(d);
             break;
         default:
-            e = $("<h2>").append(
-                "ERROR: Unknown widget type " + activeWidget.widget_type
-            );
+            e = $("<h2>")
+                .attr("class", "main-center-card card")
+                .append(
+                    "ERROR: Unknown widget type " + activeWidget.widget_type
+                );
             break;
     }
     container.empty().append(e);
